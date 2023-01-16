8a49d23aaaf22e63e60ee8ea5643ee0db1c23e60
Vincent Kipngetich <vinniebluz@icloud.com>
2023-01-16 21:09:13 +0300

    Added UsersList, UserItem components and it's styling

--- client/src/user/pages/Users.js
+++ client/src/user/pages/Users.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return (
-    <h1>Users</h1>
-  )
-}
+  const USERS = [
+    {
+      id: "u1",
+      name: "Chris Brown",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Chris_Brown_5%2C_2012.jpg/440px-Chris_Brown_5%2C_2012.jpg",
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
+};
 
 export default Users;
