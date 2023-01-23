8bec738e0f6230f8cbdd887f7b2cfd22990a55aa
Vincent Kipngetich <vinniebluz@icloud.com>
2023-01-23 20:59:37 +0300

    Implemented basic routing

--- server/app.js
+++ server/app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const placesRoutes = require("./routes/places-routes");
+
 const app = express();
+
+
+
+app.use(placesRoutes);
 
 app.listen(5001, () => console.log("Listening at 5001"));
