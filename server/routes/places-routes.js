bdc83ef1f819235ae20c43db06a09454c4f08a62
Vincent Kipngetich <vinniebluz@icloud.com>
2023-01-23 21:32:02 +0300

    Added error handling

--- server/routes/places-routes.js
+++ server/routes/places-routes.js
@@ -35,6 +35,12 @@ router.get("/:pid", (req, res, next) => {
   const placeId = req.params.pid;
 
   const place = DUMMY_PLACES.find((place) => place.id === placeId);
+
+  if (!place) {
+    const error = new Error("Could not find a place for the provided id.");
+    error.code = 404;
+    throw error;
+  }
   res.json({ place });
 });
 
@@ -42,7 +48,12 @@ router.get("/user/:uid", (req, res, next) => {
   const userId = req.params.uid;
 
   const userPlaces = DUMMY_PLACES.find((user) => user.creator === userId);
-  res.json({userPlaces})
+  if (!userPlaces) {
+    const error = new Error("Could not find a place for the provided user id.");
+    error.code = 404;
+    return next(error);
+  }
+  res.json({ userPlaces });
 });
 
 module.exports = router;
