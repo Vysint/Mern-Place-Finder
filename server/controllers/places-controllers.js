1b26c76eb7efe26c6c188520f8b2564dd0996964
Vincent Kipngetich <vinniebluz@icloud.com>
2023-01-26 22:27:19 +0300

    Code cleanup

--- server/controllers/places-controllers.js
+++ server/controllers/places-controllers.js
@@ -62,7 +62,7 @@ exports.getPlacesByUserId = (req, res, next) => {
 exports.createPlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    next(new HttpError("Invalid inputs passed, please check your data.", 422));
+    return next(new HttpError("Invalid inputs passed, please check your data.", 422));
   }
   const { title, description, address, creator } = req.body;
 
