439f68de56f914ad6a07208e25468dd65cba79a3
Vincent Kipngetich <vinniebluz@icloud.com>
2023-01-26 22:26:08 +0300

    Added Google Geocoding API to convert an address into coordinates

--- server/controllers/places-controllers.js
+++ server/controllers/places-controllers.js
@@ -1,6 +1,8 @@
-const HttpError = require("../models/http-error");
 const { v4: uuidv4 } = require("uuid");
 const { validationResult } = require("express-validator");
+
+const HttpError = require("../models/http-error");
+const getCoordsForAddress = require("../util/location");
 
 let DUMMY_PLACES = [
   {
@@ -57,14 +59,20 @@ exports.getPlacesByUserId = (req, res, next) => {
   res.json({ userPlaces });
 };
 
-exports.createPlace = (req, res, next) => {
+exports.createPlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    console.log(errors);
-    throw new HttpError("Invalid inputs passed, please check your data.", 422);
+    next(new HttpError("Invalid inputs passed, please check your data.", 422));
   }
-  const { title, description, coordinates, address, creator } = req.body;
+  const { title, description, address, creator } = req.body;
 
+  let coordinates;
+
+  try {
+    coordinates = await getCoordsForAddress(address);
+  } catch (error) {
+    return next(error);
+  }
   const createdPlace = {
     id: uuidv4(),
     title,
