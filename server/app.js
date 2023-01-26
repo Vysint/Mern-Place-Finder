5d1b1fa1792a28bffbeb45f97fb97c7f2a6e69ce
Vincent Kipngetich <vinniebluz@icloud.com>
2023-01-26 18:50:22 +0300

    Set up user routes (Login, Signup & Get Users

--- server/app.js
+++ server/app.js
@@ -4,11 +4,15 @@ const HttpError = require("./models/http-error");
 
 const placesRoutes = require("./routes/places-routes");
 
+const usersRoutes = require("./routes/users-routes")
+
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use("/api/places", placesRoutes);
+
+app.use("/api/users", usersRoutes)
 
 app.use((req, res, next) => {
   const error = new HttpError("Could not find the route", 404);
