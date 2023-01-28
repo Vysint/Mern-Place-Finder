f861a328ff3f5e39e2c8b7cc2a4984b95c43502b
Vincent Kipngetich <vinniebluz@icloud.com>
2023-01-28 19:11:32 +0300

    Connected the mongoDB via mongoose

--- server/app.js
+++ server/app.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
 const HttpError = require("./models/http-error");
-
 const placesRoutes = require("./routes/places-routes");
-
-const usersRoutes = require("./routes/users-routes")
+const usersRoutes = require("./routes/users-routes");
 
 const app = express();
 
 app.use(bodyParser.json());
+dotenv.config();
 
 app.use("/api/places", placesRoutes);
 
-app.use("/api/users", usersRoutes)
+app.use("/api/users", usersRoutes);
 
 app.use((req, res, next) => {
   const error = new HttpError("Could not find the route", 404);
@@ -27,4 +29,14 @@ app.use((error, req, res, next) => {
     .status(error.code || 500)
     .json({ message: error.message || "An unknown error occurred!" });
 });
-app.listen(5001, () => console.log("Listening at 5001"));
+
+mongoose.set('strictQuery', false)
+
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then(() => {
+    app.listen(process.env.PORT, () =>
+      console.log(`Listening at ${process.env.PORT}`)
+    );
+  })
+  .catch((err) => console.log(err));
