4c5a4216d7423a8f4be9c9f2b77b4781585d6d88
Vincent Kipngetich <vinniebluz@icloud.com>
2023-01-29 14:34:53 +0300

    Added functionality to deletePlaces in the database -- finished on the CRUD operations for places

--- server/controllers/places-controllers.js
+++ server/controllers/places-controllers.js
@@ -54,7 +54,7 @@ exports.getPlaceById = async (req, res, next) => {
   res.json({ place: place.toObject({ getters: true }) });
 };
 
-exports.getPlacesByUserId = async(req, res, next) => {
+exports.getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
 
   let userPlaces;
@@ -157,11 +157,28 @@ exports.updatePlace = async (req, res, next) => {
 
   res.status(200).json({ place: place.toObject({ getters: true }) });
 };
-exports.deletePlace = (req, res, next) => {
+exports.deletePlace = async (req, res, next) => {
   const placeId = req.params.pid;
-  if (!DUMMY_PLACES.find((place) => place.id === placeId)) {
-    throw new HttpError("Could not find a place for that id.", 404);
+  let place;
+
+  try {
+    place = await Place.findById(placeId);
+  } catch (err) {
+    const error = new HttpError(
+      "Something went wrong, could not delete place.",
+      500
+    );
+    return next(error);
   }
-  DUMMY_PLACES = DUMMY_PLACES.filter((place) => place.id !== placeId);
+
+  try {
+    await place.remove();
+  } catch (err) {
+    const error = new HttpError(
+      "Something went wrong, could not delete place.",
+      500
+    );
+    return next(error);
+  }
   res.status(200).json({ message: "Deleted place." });
 };
