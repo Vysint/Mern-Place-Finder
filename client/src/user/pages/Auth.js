78a3e6e6ede0fe253de5bbb4166f364383c4df95
Vincent Kipngetich <vinniebluz@icloud.com>
2023-02-06 14:27:28 +0300

    Sent a Login request

--- client/src/user/pages/Auth.js
+++ client/src/user/pages/Auth.js
@@ -1,9 +1,8 @@
-import React, { useContext, useState } from "react";
-import Button from "../../shared/components/FormElements/Button";
-import Input from "../../shared/components/FormElements/Input";
+import React, { useState, useContext } from "react";
+
 import Card from "../../shared/components/UIElements/Card";
-import { AuthContext } from "../../shared/context/auth-context";
-import { useForm } from "../../shared/hooks/form-hook";
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 import {
@@ -11,14 +10,16 @@ import {
   VALIDATOR_MINLENGTH,
   VALIDATOR_REQUIRE,
 } from "../../shared/util/validators";
-
+import { useForm } from "../../shared/hooks/form-hook";
+import { AuthContext } from "../../shared/context/auth-context";
 import "./Auth.css";
 
 const Auth = () => {
   const auth = useContext(AuthContext);
-  const [isLoginMode, setIsLoginMode] = useState(false);
+  const [isLoginMode, setIsLoginMode] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
+
   const [formState, inputHandler, setFormData] = useForm(
     {
       email: {
@@ -33,39 +34,6 @@ const Auth = () => {
     false
   );
 
-  const authSubmitHandler = async (event) => {
-    event.preventDefault();
-
-    if (isLoginMode) {
-    } else {
-      try {
-        setIsLoading(true);
-        const response = await fetch("http://localhost:5001/api/users/signup", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            name: formState.inputs.name.value,
-            email: formState.inputs.email.value,
-            password: formState.inputs.password.value,
-          }),
-        });
-        const responseData = await response.json();
-
-        if (!response.ok) {
-          throw new Error(responseData.message);
-        }
-        console.log(responseData);
-        setIsLoading(false);
-        auth.login();
-      } catch (err) {
-        console.log(err);
-        setIsLoading(false);
-        setError(err.message || "Something went wrong, please try again.");
-      }
-    }
-  };
   const switchModeHandler = () => {
     if (!isLoginMode) {
       setFormData(
@@ -87,12 +55,68 @@ const Auth = () => {
         false
       );
     }
-    setIsLoginMode((prevLoginMode) => !prevLoginMode);
+    setIsLoginMode((prevMode) => !prevMode);
+  };
+
+  const authSubmitHandler = async (event) => {
+    event.preventDefault();
+
+    setIsLoading(true);
+
+    if (isLoginMode) {
+      try {
+        const response = await fetch("http://localhost:5001/api/users/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        });
+
+        const responseData = await response.json();
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+        setIsLoading(false);
+        auth.login();
+      } catch (err) {
+        setIsLoading(false);
+        setError(err.message || "Something went wrong, please try again.");
+      }
+    } else {
+      try {
+        const response = await fetch("http://localhost:5001/api/users/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: formState.inputs.name.value,
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        });
+
+        const responseData = await response.json();
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+        setIsLoading(false);
+        auth.login();
+      } catch (err) {
+        setIsLoading(false);
+        setError(err.message || "Something went wrong, please try again.");
+      }
+    }
   };
 
   const errorHandler = () => {
     setError(null);
   };
+
   return (
     <React.Fragment>
       <ErrorModal error={error} onClear={errorHandler} />
@@ -106,24 +130,24 @@ const Auth = () => {
               element="input"
               id="name"
               type="text"
-              label="Your name"
+              label="Your Name"
               validators={[VALIDATOR_REQUIRE()]}
-              errorText="Please enter your name."
+              errorText="Please enter a name."
               onInput={inputHandler}
             />
           )}
           <Input
-            id="email"
             element="input"
+            id="email"
             type="email"
-            label="Email"
-            validators={[VALIDATOR_EMAIL]}
+            label="E-Mail"
+            validators={[VALIDATOR_EMAIL()]}
             errorText="Please enter a valid email address."
             onInput={inputHandler}
           />
           <Input
-            id="password"
             element="input"
+            id="password"
             type="password"
             label="Password"
             validators={[VALIDATOR_MINLENGTH(5)]}
