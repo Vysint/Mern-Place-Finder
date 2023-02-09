d4f45627d4d4a5300ec30e791461c902c329c5ba
Vincent Kipngetich <vinniebluz@icloud.com>
2023-02-09 16:17:23 +0300

    Added state management for the User id

--- client/src/App.js
+++ client/src/App.js
@@ -15,11 +15,15 @@ import Users from "./user/pages/Users";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const login = useCallback(() => {
+  const [userId, setUserId] = useState(false);
+
+  const login = useCallback((uid) => {
     setIsLoggedIn(true);
+    setUserId(uid);
   }, []);
   const logout = useCallback(() => {
     setIsLoggedIn(false);
+    setUserId(null);
   }, []);
 
   let routes;
@@ -60,7 +64,12 @@ function App() {
   }
   return (
     <AuthContext.Provider
-      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+      value={{
+        isLoggedIn: isLoggedIn,
+        userId: userId,
+        login: login,
+        logout: logout,
+      }}
     >
       <Router>
         <MainNavigation />
