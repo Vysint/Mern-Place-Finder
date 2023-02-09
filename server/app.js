c7f859a4964b0424191eaa7863513895f881490e
Vincent Kipngetich <vinniebluz@icloud.com>
2023-02-10 01:39:41 +0300

    Solved CORS issue

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 const HttpError = require("./models/http-error");
 const placesRoutes = require("./routes/places-routes");
@@ -12,6 +13,7 @@ const app = express();
 app.use(bodyParser.json());
 dotenv.config();
 
+app.use(cors());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
