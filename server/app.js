18e3f0ccda54d6ed5a31189b809bc10d3a8329e1
Vincent Kipngetich <vinniebluz@icloud.com>
2023-02-12 00:21:49 +0300

    Served images statically

--- server/app.js
+++ server/app.js
@@ -1,4 +1,6 @@
 const fs = require("fs");
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -12,6 +14,7 @@ const usersRoutes = require("./routes/users-routes");
 const app = express();
 
 app.use(bodyParser.json());
+app.use("/uploads/images", express.static(path.join("uploads", "images")));
 dotenv.config();
 
 app.use(cors());
