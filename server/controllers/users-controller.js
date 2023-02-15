fa01dd91f7067af72f0102a1872d30b2eecf905f
Vincent Kipngetich <vinniebluz@icloud.com>
2023-02-15 17:25:55 +0300

    Attached JWT (tokens) in React

--- server/controllers/users-controller.js
+++ server/controllers/users-controller.js
@@ -20,16 +20,15 @@ exports.getUsers = async (req, res, next) => {
 
 exports.signUp = async (req, res, next) => {
   const errors = validationResult(req);
-
   if (!errors.isEmpty()) {
     return next(
       new HttpError("Invalid inputs passed, please check your data.", 422)
     );
   }
+
   const { name, email, password } = req.body;
 
   let existingUser;
-
   try {
     existingUser = await User.findOne({ email: email });
   } catch (err) {
@@ -49,30 +48,35 @@ exports.signUp = async (req, res, next) => {
   }
 
   let hashedPassword;
-
   try {
-    await bcrypt.hash(password, 12);
+    hashedPassword = await bcrypt.hash(password, 12);
   } catch (err) {
-    return next(new HttpError("Could not create user, please try again.", 500));
+    const error = new HttpError(
+      "Could not create user, please try again.",
+      500
+    );
+    return next(error);
   }
 
   const createdUser = new User({
     name,
     email,
-    password: hashedPassword,
     image: req.file.path,
+    password: hashedPassword,
     places: [],
   });
 
   try {
     await createdUser.save();
   } catch (err) {
-    const error = new HttpError("Signing up failed, please try again.", 500);
+    const error = new HttpError(
+      "Signing up failed, please try again later.",
+      500
+    );
     return next(error);
   }
 
   let token;
-
   try {
     token = jwt.sign(
       { userId: createdUser.id, email: createdUser.email },
@@ -80,9 +84,11 @@ exports.signUp = async (req, res, next) => {
       { expiresIn: "1h" }
     );
   } catch (err) {
-    return next(
-      new HttpError("Signing up failed, please try again later.", 500)
+    const error = new HttpError(
+      "Signing up failed, please try again later.",
+      500
     );
+    return next(error);
   }
 
   res
@@ -93,57 +99,63 @@ exports.signUp = async (req, res, next) => {
 exports.login = async (req, res, next) => {
   const { email, password } = req.body;
 
-  let identifiedUser;
+  let existingUser;
 
   try {
-    identifiedUser = await User.findOne({ email: email });
+    existingUser = await User.findOne({ email: email });
   } catch (err) {
-    return next(
-      new HttpError("Logging in failed, please try again later.", 500)
+    const error = new HttpError(
+      "Logging in failed, please try again later.",
+      500
     );
+    return next(error);
   }
 
-  if (!identifiedUser) {
-    return next(
-      new HttpError(
-        "Could not identify user, credentials seem to be wrong.",
-        401
-      )
+  if (!existingUser) {
+    const error = new HttpError(
+      "Invalid credentials, could not log you in.",
+      401
     );
+    return next(error);
   }
 
-  let isValidPassword;
+  let isValidPassword = false;
   try {
-    isValidPassword = await bcrypt.compare(password, identifiedUser.password);
+    isValidPassword = await bcrypt.compare(password, existingUser.password);
   } catch (err) {
-    return next(
-      new HttpError(
-        "Could not log you in, please check your credentials and try again.",
-        500
-      )
+    const error = new HttpError(
+      "Could not log you in, please check your credentials and try again.",
+      500
     );
+    return next(error);
   }
+
   if (!isValidPassword) {
-    return next(
-      new HttpError("Invalid credentials, could not log you in.", 500)
+    const error = new HttpError(
+      "Invalid credentials, could not log you in.",
+      401
     );
+    return next(error);
   }
-  let token;
 
+  let token;
   try {
     token = jwt.sign(
-      { userId: identifiedUser.id, email: identifiedUser.email },
+      { userId: existingUser.id, email: existingUser.email },
       "supersecret_dont_share",
       { expiresIn: "1h" }
     );
   } catch (err) {
-    return next(
-      new HttpError("Logging in failed, please try again later.", 500)
+    const error = new HttpError(
+      "Logging in failed, please try again later.",
+      500
     );
+    return next(error);
   }
+
   res.json({
-    userId: identifiedUser.id,
-    email: identifiedUser.email,
+    userId: existingUser.id,
+    email: existingUser.email,
     token: token,
   });
 };
