2b5e50bb265b5d24ca6f007f22d220f856dd9bb8
Vincent Kipngetich <vinniebluz@icloud.com>
2023-02-16 19:20:14 +0300

    General code improvement

--- server/controllers/places-controller.js
+++ server/controllers/places-controller.js
@@ -62,7 +62,7 @@ exports.createPlace = async (req, res, next) => {
       new HttpError("Invalid inputs passed, please check your data.", 422)
     );
   }
-  const { title, description, address, creator } = req.body;
+  const { title, description, address } = req.body;
 
   let coordinates;
 
@@ -77,12 +77,12 @@ exports.createPlace = async (req, res, next) => {
     address,
     location: coordinates,
     image: req.file.path,
-    creator,
+    creator: req.userData.userId,
   });
   let user;
 
   try {
-    user = await User.findById(creator);
+    user = await User.findById(req.userData.userId);
   } catch (err) {
     return next(new HttpError("Creating place failed, please try again.", 500));
   }
